c9f39668c59b878f34c0acdf13ec8c0b4dc21a57
MoringaSchool <you@example.com>
2020-01-17 11:53:37 +0300

    first commit

---
+++ src/test/java/models/FoodtypeTest.java
@@ -0,0 +1,47 @@
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FoodtypeTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getName() {
+        Foodtype testFoodtype = setupFoodtype();
+        assertEquals("dessert", testFoodtype.getName());
+    }
+
+    @Test
+    public void setName() {
+        Foodtype testFoodtype = setupFoodtype();
+        testFoodtype.setName("breakfast");
+        assertNotEquals("dessert", testFoodtype.getName());
+    }
+
+    @Test
+    public void getId() {
+        Foodtype testFoodtype = setupFoodtype();
+        testFoodtype.setId(5);
+        assertEquals(5, testFoodtype.getId());
+    }
+
+
+
+    @Test
+    public Foodtype setupFoodtype() {
+        return new Foodtype("dessert");
+    }
+
+
+}
